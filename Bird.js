442d4d27b99f4af7483df206175c606326e3370b
Neelng7 <neelnaraingupta@gmail.com>
2020-01-05 18:59:03 +0530

    efrrh

--- Bird.js
+++ Bird.js
@@ -1,13 +1,49 @@
 class Bird extends BaseClass {
-  constructor(x,y){
-    super(x,y,50,50);
-    this.image = loadImage("sprites/bird.png");
-  }
-
-  display() {
-    //this.body.position.x = mouseX;
-    //this.body.position.y = mouseY;
-
-    super.display();
-  }
-}
+   constructor(x,y){ 
+super(x,y,50,50);
+this.image = loadImage("sprites/bird.png");
+ this.smokeImage = loadImage("sprites/smoke.png"); 
+ this.trajectory =[];
+ } 
+ 
+ display() {
+    super.display(); 
+    if(this.body.velocity.x > 20 && this.body.position.x > 200){
+     var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+     }
+      for(var i=0; i<this.trajectory.length; i++){ 
+        image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+       }
+       }
+       }
+       /*class Bird extends BaseClass {
+        constructor(x,y){
+          super(x,y,50,50);
+          this.image = loadImage("sprites/bird.png");
+          this.smoke = loadImage("sprites/smoke.png");
+        
+         this.trajectory = [];
+      
+      
+        }
+      
+        display() {
+          super.display();
+      
+          console.log(this.body.velocity.x);
+      
+          if(this.body.velocity.x>20 && this.body.position.x>200){
+            var position = [this.body.position.x,this.body.position.y];
+            this.trajectory.push(position);
+          }
+      
+          for(var i  = 0; i < this.trajectory.length; i++){
+      
+          image(this.smoke,this.trajectory[i][0],this.trajectory [i][0]);
+          
+        } 
+      
+        }
+      }*/
+      
